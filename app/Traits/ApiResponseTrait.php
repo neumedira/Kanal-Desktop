80f58d4e1dad032f070c15f518fda862a6b28d79
ihyaCantik <ihya010100@gmail.com>
2026-09-03 14:39:48 +0800

    menambahkan ApiResponsTrait.php

---
+++ app/Traits/ApiResponseTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponseTrait
+{
+    public function successResponse($data, $message = 'Success', $code = 200)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+
+    public function errorResponse($message = 'Error', $errors = null, $code = 400)
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'errors' => $errors,
+        ], $code);
+    }
+}
