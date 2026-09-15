111df4828a429dbbbccf14c54e9452616bca3376
Errllo <talipazahrannailah@gmail.com>
2026-09-15 14:59:53 +0800

    update settingcontroller

--- app/Http/Requests/Api/Bonus/UpdateSettingRequest.php
+++ app/Http/Requests/Api/Bonus/UpdateSettingRequest.php
@@ -2,28 +2,20 @@
 
 namespace App\Http\Requests\Api\Bonus;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateSettingRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true; // <-- Wajib true agar diizinkan lewat
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'minimal_views' => 'required|integer|min:0',
+            'nominal_bonus' => 'required|numeric|min:0',
         ];
     }
 }
